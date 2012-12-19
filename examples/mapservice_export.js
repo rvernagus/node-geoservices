847dd1473259d626e69b2d40c22d6ed1ad32602f
Ray Vernagus <r.vernagus@gmail.com>
2012-12-19 16:58:43 -0500

    Added mapservice export example.

---
+++ examples/mapservice_export.js
@@ -0,0 +1,17 @@
+var geoservices = require('../');
+
+var options = {
+  host: "sampleserver1.arcgisonline.com",
+  path: "/ArcGIS/rest/services/Specialty/ESRI_StateCityHighway_USA/MapServer/export",
+  params: {
+    bbox: "-115.8,30.4,-85.5,50.5",
+    size: "800,600",
+    imageSR: 102004,
+    format: "gif",
+    transparent: false
+  }
+};
+
+geoservices.get(options, function(result) {
+  console.log(result);
+});
