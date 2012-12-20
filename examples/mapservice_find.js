d8da58599871f54493f742e70e4d6ef3be8981ed
Ray Vernagus <r.vernagus@gmail.com>
2012-12-19 23:16:17 -0500

    Added mapservice find example.

---
+++ examples/mapservice_find.js
@@ -0,0 +1,16 @@
+var geoservices = require('../');
+
+var options = {
+  host: "sampleserver1.arcgisonline.com",
+  path: "/ArcGIS/rest/services/Specialty/ESRI_StatesCitiesRivers_USA/MapServer/find",
+  params: {
+    searchText: "island",
+    contains: true,
+    layers: "0,2",
+    returnGeometry: true
+  }
+};
+
+geoservices.get(options, function(result) {
+  console.log(result);
+});
