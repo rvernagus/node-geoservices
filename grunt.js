c781217b3e24e0d584640bc0ac4ed619a815f9b2
Ray Vernagus <r.vernagus@gmail.com>
2013-01-20 17:57:22 -0500

    Use coffeelint.

--- grunt.js
+++ grunt.js
@@ -1,9 +1,17 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-jslint');
+  grunt.loadNpmTasks('grunt-coffeelint');
   grunt.initConfig({
-    lint: {
-      files: ['*.js', 'lib/**/*.js', 'test/**/*.js']
+    coffeelint: {
+      app: ['*.coffee', 'lib/**/*.coffee'],
+      tests: {
+        files: {
+          src: ['test/**/*.coffee']
+        },
+        options: {
+          "max_line_length": 120
+        }
+      }
     }
   });
-  grunt.registerTask('default', 'lint');
+  grunt.registerTask('default', 'coffeelint');
 };
