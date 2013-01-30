c2a26fd2a7e5a8abfd7f4133972425fbb0a5cb68
Ray Vernagus <r.vernagus@gmail.com>
2013-01-29 21:33:34 -0500

    Added tests for arrays of features.

--- test/geoservices.converter.test.coffee
+++ test/geoservices.converter.test.coffee
@@ -8,6 +8,10 @@ describe "geoservices converter", ->
         attributes:
           attr1: "val1"
 
+    it "should error upon empty geometry", ->
+      @esriFeature.geometry = {}
+      assert.throws (-> geoservices.convert.toGeoJSON @esriFeature), Error
+
     it "should convert a point", ->
       @esriFeature.geometry = {x:1, y:2}
       result = geoservices.convert.toGeoJSON @esriFeature
@@ -19,10 +23,6 @@ describe "geoservices converter", ->
         properties:
           attr1: "val1"
       assert.deepEqual result, expected
-
-    it "should error upon empty point", ->
-      @esriFeature.geometry = {}
-      assert.throws (-> geoservices.convert.toGeoJSON @esriFeature), Error
 
     it "should convert an array of points", ->
       @esriFeature.geometry = {x:1, y:2}
@@ -50,7 +50,7 @@ describe "geoservices converter", ->
       assert.equal 2, result.length
       assert.equal "MultiPoint", result[0].geometry.type
 
-    it "should convert a one path polyline", ->
+    it "should convert a one-path polyline", ->
       @esriFeature.geometry = {paths: [[[1,2],[3,4]]]}
       result = geoservices.convert.toGeoJSON @esriFeature
       expected =
@@ -62,7 +62,14 @@ describe "geoservices converter", ->
           attr1: "val1"
       assert.deepEqual result, expected
 
-    it "should convert a two path polyline", ->
+    it "should convert an array of one-path polylines", ->
+      @esriFeature.geometry = {paths: [[[1,2],[3,4]]]}
+      esriFeatures = [@esriFeature, @esriFeature]
+      result = geoservices.convert.toGeoJSON esriFeatures
+      assert.equal 2, result.length
+      assert.equal "LineString", result[0].geometry.type
+
+    it "should convert a two-path polyline", ->
       @esriFeature.geometry = {paths: [[[1,2],[3,4]],[[5,6],[7,8]]]}
       result = geoservices.convert.toGeoJSON @esriFeature
       expected =
@@ -73,6 +80,13 @@ describe "geoservices converter", ->
         properties:
           attr1: "val1"
       assert.deepEqual result, expected
+
+    it "should convert an array of two-path polylines", ->
+      @esriFeature.geometry = {paths: [[[1,2],[3,4]],[[5,6],[7,8]]]}
+      esriFeatures = [@esriFeature, @esriFeature]
+      result = geoservices.convert.toGeoJSON esriFeatures
+      assert.equal 2, result.length
+      assert.equal "MultiLineString", result[0].geometry.type
 
     it "should convert a single ring polygon", ->
       @esriFeature.geometry = {rings: [[[1,2],[3,4]],[[5,6],[1,2]]]}
--- test/geoservices.converter.test.js
+++ test/geoservices.converter.test.js
@@ -13,6 +13,12 @@ describe("geoservices converter", function() {
         }
       };
     });
+    it("should error upon empty geometry", function() {
+      this.esriFeature.geometry = {};
+      return assert.throws((function() {
+        return geoservices.convert.toGeoJSON(this.esriFeature);
+      }), Error);
+    });
     it("should convert a point", function() {
       var expected, result;
       this.esriFeature.geometry = {
@@ -31,12 +37,6 @@ describe("geoservices converter", function() {
         }
       };
       return assert.deepEqual(result, expected);
-    });
-    it("should error upon empty point", function() {
-      this.esriFeature.geometry = {};
-      return assert.throws((function() {
-        return geoservices.convert.toGeoJSON(this.esriFeature);
-      }), Error);
     });
     it("should convert an array of points", function() {
       var esriFeatures, result;
@@ -77,7 +77,7 @@ describe("geoservices converter", function() {
       assert.equal(2, result.length);
       return assert.equal("MultiPoint", result[0].geometry.type);
     });
-    it("should convert a one path polyline", function() {
+    it("should convert a one-path polyline", function() {
       var expected, result;
       this.esriFeature.geometry = {
         paths: [[[1, 2], [3, 4]]]
@@ -95,7 +95,17 @@ describe("geoservices converter", function() {
       };
       return assert.deepEqual(result, expected);
     });
-    it("should convert a two path polyline", function() {
+    it("should convert an array of one-path polylines", function() {
+      var esriFeatures, result;
+      this.esriFeature.geometry = {
+        paths: [[[1, 2], [3, 4]]]
+      };
+      esriFeatures = [this.esriFeature, this.esriFeature];
+      result = geoservices.convert.toGeoJSON(esriFeatures);
+      assert.equal(2, result.length);
+      return assert.equal("LineString", result[0].geometry.type);
+    });
+    it("should convert a two-path polyline", function() {
       var expected, result;
       this.esriFeature.geometry = {
         paths: [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
@@ -112,6 +122,16 @@ describe("geoservices converter", function() {
         }
       };
       return assert.deepEqual(result, expected);
+    });
+    it("should convert an array of two-path polylines", function() {
+      var esriFeatures, result;
+      this.esriFeature.geometry = {
+        paths: [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+      };
+      esriFeatures = [this.esriFeature, this.esriFeature];
+      result = geoservices.convert.toGeoJSON(esriFeatures);
+      assert.equal(2, result.length);
+      return assert.equal("MultiLineString", result[0].geometry.type);
     });
     return it("should convert a single ring polygon", function() {
       var expected, result;
